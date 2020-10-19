1e64ad83be17c69ae392b8f7d7a3d2421470c88b
timurka169 <timurka335@gmail.com>
2020-10-19 17:07:26 +0300

    Initial commit

---
+++ src/main/scala/quickcheck/QuickCheck.scala
@@ -0,0 +1,24 @@
+package quickcheck
+
+import org.scalacheck._
+import Arbitrary._
+import Gen._
+import Prop._
+
+
+abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
+
+
+  lazy val genHeap: Gen[H] = for {
+    n <- arbitrary[A]
+    h <- frequency((1, Gen.const(empty)), (9, genHeap))
+  } yield insert(n, h)//??
+
+  implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
+
+  property("gen1") = forAll { (h: H) =>
+    val m = if (isEmpty(h)) 0 else findMin(h)
+    findMin(insert(m, h)) == m
+  }
+
+}
